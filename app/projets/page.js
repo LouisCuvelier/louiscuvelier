a99402fc643cde01ccdfd3551f2e0825595f3c1f
Louis <hello@louiscuvelier.com>
2023-07-18 17:25:35 +0200

    Added animation

--- app/projets/page.js
+++ app/projets/page.js
@@ -1,8 +1,5 @@
-import Link from "next/link";
 import { getSortedProjectsData } from "../../lib/projects";
-import Image from "next/image";
-import ArrowUpRight from "../../public/images/arrow-up-right.svg";
-import Clock from "../../public/images/clock.svg";
+import ProjectCard from "../../components/ProjectCard";
 export default function Projects() {
   const allProjectsData = getSortedProjectsData();
 
@@ -13,116 +10,9 @@ export default function Projects() {
       </section>
 
       <section className={"flex flex-col gap-16 mx-auto"}>
-        {allProjectsData.map(
-          (
-            {
-              id,
-              client,
-              description,
-              website,
-              categories,
-              work,
-              duration,
-              technologies,
-              logo,
-              cover,
-            },
-            index
-          ) => (
-            <>
-              <article
-                key={id}
-                className={"border-hatch @container border-[12px] rounded"}
-              >
-                <div className={"w-full relative"}>
-                  <Image
-                    src={cover}
-                    className={"rounded-t"}
-                    width={5312}
-                    height={2724}
-                    alt={`Image de couverture de ${client}`}
-                  />
-                  <ul
-                    className={
-                      "absolute right-12 bottom-12 z-10 gap-2 flex flex-row flex-wrap mt-1"
-                    }
-                  >
-                    {categories.map((value, index) => (
-                      <li
-                        key={index}
-                        className={
-                          "uppertitle uppertitle-2 bg-hatch-secondary shadow-blue-950 rounded pl-3 pr-2 py-1 bg-blue-100/50 text-white shadow-xl"
-                        }
-                      >
-                        {value}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-
-                <div className={"p-14"}>
-                  <div className={"@xl:gap-14 @xl:grid @xl:grid-cols-2"}>
-                    <div>
-                      <div
-                        className={"flex justify-between items-stretch mb-14"}
-                      >
-                        <div>
-                          <div
-                            className={
-                              "uppertitle uppertitle-2 flex text-slate-500"
-                            }
-                          >
-                            <Clock className={"mr-2 h-5 w-5"} />
-                            {duration}
-                          </div>
-                          <h2 className={"title title-2 mt-8 mb-4"}>
-                            {client}
-                          </h2>
-                          <Link
-                            href={website}
-                            className={"btn btn-tertiary"}
-                            rel={"noopener noreferrer"}
-                            target={"_blank"}
-                          >
-                            <span>Voir le projet</span>
-                            <ArrowUpRight />
-                          </Link>
-                        </div>
-                      </div>
-                      <div>
-                        <p className={"subtitle subtitle-2 mb-14 @lg:mb-0"}>
-                          {work}
-                        </p>
-                      </div>
-                    </div>
-
-                    <div className={"mt-16 space-y-8 self-end"}>
-                      <div>
-                        <h3 className={"title title-4 mb-3"}>Client</h3>
-                        <p className={"body body-1"}>{description}</p>
-                      </div>
-                      <div>
-                        <h3 className={"title title-4 mb-3"}>Technologies</h3>
-                        <ul className={"gap-2 flex flex-row flex-wrap mt-1"}>
-                          {technologies.map((value, index) => (
-                            <li
-                              key={index}
-                              className={
-                                "uppertitle uppertitle-2 bg-hatch-secondary rounded pl-2 pr-1"
-                              }
-                            >
-                              {value}
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </article>
-            </>
-          )
-        )}
+        {allProjectsData.map((props, index) => (
+          <ProjectCard index={index} {...props} />
+        ))}
       </section>
     </>
   );
---
+++ components/ProjectCard.js
@@ -0,0 +1,156 @@
+"use client";
+import Image from "next/image";
+import Clock from "../public/images/clock.svg";
+import Link from "next/link";
+import ArrowUpRight from "../public/images/arrow-up-right.svg";
+import { useEffect } from "react";
+
+export default function ProjectCard({
+  index,
+  id,
+  client,
+  description,
+  website,
+  categories,
+  work,
+  duration,
+  technologies,
+  cover,
+}) {
+  useEffect(() => {
+    // Sélectionnez toutes les cartes avec la classe "projects"
+    const projects = document.querySelectorAll(".projects");
+
+    // Fonction pour effectuer l'inclinaison et la mise à l'échelle de la carte en fonction de la position de la souris
+    function rotateToMouse(e) {
+      const bounds = e.currentTarget.getBoundingClientRect(); // Récupérez les dimensions de la carte en utilisant getBoundingClientRect()
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+      const leftX = mouseX - bounds.x;
+      const topY = mouseY - bounds.y;
+      const center = {
+        x: leftX - bounds.width / 2,
+        y: topY - bounds.height / 2,
+      };
+      const distance = Math.sqrt(center.x ** 2 + center.y ** 2);
+
+      // Mettre à l'échelle et incliner la carte en fonction de la position de la souris
+      e.currentTarget.style.transform = `
+    scale3d(1.05, 1.05, 1.05)
+    rotate3d(
+      ${center.y / 100},
+      ${-center.x / 100},
+      0,
+      ${Math.log(distance) * 2}deg
+    )
+  `;
+    }
+
+    // Fonction pour gérer l'événement "mouseenter" (souris entre dans la carte)
+    function handleMouseEnter(e) {
+      // Ajoutez l'écouteur d'événement "mousemove" pour suivre la souris lorsque la souris est sur la carte
+      e.currentTarget.addEventListener("mousemove", rotateToMouse);
+    }
+
+    // Fonction pour gérer l'événement "mouseleave" (souris quitte la carte)
+    function handleMouseLeave(e) {
+      // Supprimez l'écouteur d'événement "mousemove" pour arrêter de suivre la souris lorsque la souris quitte la carte
+      e.currentTarget.removeEventListener("mousemove", rotateToMouse);
+      // Réinitialisez la transformation de la carte à sa position d'origine
+      e.currentTarget.style.transform = "";
+    }
+
+    // Ajoutez les écouteurs d'événement pour chaque carte avec la classe "projects"
+    projects.forEach((project) => {
+      // Lorsque la souris entre dans la carte, appelez handleMouseEnter
+      project.addEventListener("mouseenter", handleMouseEnter);
+      // Lorsque la souris quitte la carte, appelez handleMouseLeave
+      project.addEventListener("mouseleave", handleMouseLeave);
+    });
+  });
+
+  return (
+    <article
+      key={index}
+      className={
+        "projects border-hatch duration-800 @container border-[12px] rounded hover:shadow-lg"
+      }
+    >
+      <div className={"w-full relative"}>
+        <Image
+          src={cover}
+          className={"rounded-t"}
+          width={5312}
+          height={2724}
+          alt={`Image de couverture de ${client}`}
+        />
+        <ul
+          className={
+            "absolute right-12 bottom-12 z-10 gap-2 flex flex-row flex-wrap mt-1"
+          }
+        >
+          {categories.map((value, index) => (
+            <li
+              key={index}
+              className={
+                "uppertitle uppertitle-2 bg-hatch-secondary shadow-blue-950 rounded pl-3 pr-2 py-1 bg-blue-100/50 text-white shadow-xl"
+              }
+            >
+              {value}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className={"p-14"}>
+        <div className={"@xl:gap-14 @xl:grid @xl:grid-cols-2"}>
+          <div>
+            <div className={"flex justify-between items-stretch mb-14"}>
+              <div>
+                <div className={"uppertitle uppertitle-2 flex text-slate-500"}>
+                  <Clock className={"mr-2 h-5 w-5"} />
+                  {duration}
+                </div>
+                <h2 className={"title title-2 mt-8 mb-4"}>{client}</h2>
+                <Link
+                  href={website}
+                  className={"btn btn-tertiary"}
+                  rel={"noopener noreferrer"}
+                  target={"_blank"}
+                >
+                  <span>Voir le projet</span>
+                  <ArrowUpRight />
+                </Link>
+              </div>
+            </div>
+            <div>
+              <p className={"subtitle subtitle-2 mb-14 @lg:mb-0"}>{work}</p>
+            </div>
+          </div>
+
+          <div className={"mt-16 space-y-8 self-end"}>
+            <div>
+              <h3 className={"title title-4 mb-3"}>Client</h3>
+              <p className={"body body-1"}>{description}</p>
+            </div>
+            <div>
+              <h3 className={"title title-4 mb-3"}>Technologies</h3>
+              <ul className={"gap-2 flex flex-row flex-wrap mt-1"}>
+                {technologies.map((value, index) => (
+                  <li
+                    key={index}
+                    className={
+                      "uppertitle uppertitle-2 bg-hatch-secondary rounded pl-2 pr-1"
+                    }
+                  >
+                    {value}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+}
